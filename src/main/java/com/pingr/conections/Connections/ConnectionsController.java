1562f85a9a7f64f3fb337ab6c08b6c79eb6bbc56
João Daniel <jotaf.daniel@gmail.com>
2022-01-26 16:20:42 -0300

    remove unused files
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- src/main/java/com/pingr/conections/Connections/ConnectionsController.java
+++ src/main/java/com/pingr/conections/Connections/ConnectionsController.java
@@ -11,20 +11,11 @@ import java.util.HashSet;
 @RestController
 @RequestMapping(path = "/connections")
 public class ConnectionsController {
-    private final FakeProducer fakeProducer;
     private final AccountService accountService;
 
     @Autowired
-    public ConnectionsController(FakeProducer fakeProducer, AccountService accountService) {
-        this.fakeProducer = fakeProducer;
+    public ConnectionsController(AccountService accountService) {
         this.accountService = accountService;
-    }
-
-    @PostMapping(path = "/test")
-    public Account fakeCreate() {
-        Account fake = new Account(12L, "fake username", new HashSet<>());
-        this.fakeProducer.sendMessage(fake);
-        return fake;
     }
 
     @PostMapping(path = "/{aid}/{bid}")
